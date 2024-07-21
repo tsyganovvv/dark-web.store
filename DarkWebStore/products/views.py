f52b0f090221422e04470132758856a0bd26acfe
Vladimir Tsyganov <153202831+tsyganovvv@users.noreply.github.com>
2024-07-21 15:20:55 +0700

    products app

--- DarkWebStore/products/views.py
+++ DarkWebStore/products/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from products.models import Product, ProductCategory
+
+
 # Create your views here.
 
 def index(request):
-    context={}
+    context={
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()}
     return render(request, 'products/index.html', context)
