40757958164b2f06cc5eba1a233da8c60f96802c
Vladimir Tsyganov <153202831+tsyganovvv@users.noreply.github.com>
2024-07-31 17:44:38 +0700

    copy fix

--- DarkWebStore/static/js/copy.js
+++ DarkWebStore/static/js/copy.js
@@ -1,3 +1,12 @@
-let copy = (copyText) => {
-    navigator.clipboard.writeText(copyText);
-}
+const copy = (text) => {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(text)
+    } else {
+      const input = document.createElement('textarea')
+      input.value = text
+      document.body.appendChild(input)
+      input.select()
+      document.execCommand('copy')
+      document.body.removeChild(input)
+    }
+}  
